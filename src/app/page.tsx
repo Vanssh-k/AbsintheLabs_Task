025e4827ab2c3a619d0450cd80519ce51d9eb6b0
the-navin <vanshkapoor2001@gmail.com>
2024-09-04 01:13:33 +0530

    Apollo client setup to fetch last activities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,23 @@ import Header from "@/components/Header";
 import HowToEarn from "@/components/HowToEarn";
 import { Theme } from "@radix-ui/themes";
 import { Box } from "@radix-ui/themes";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo-client";
+import LastActivities from "@/components/LastActivities";
 
 export default function Home() {
   return (
-    <Theme>
-      <Header />
-      <Box className="px-32 py-2">
-        {/* <HowToEarn /> */}
-        {/* <CommunityBadges /> */}
-        <BadgeCarousel />
-      </Box>
-      {/* <Footer /> */}
-    </Theme>
+    <ApolloProvider client={client}>
+      <Theme>
+        <Header />
+        <Box className="px-32 py-2">
+          {/* <HowToEarn /> */}
+          {/* <CommunityBadges /> */}
+          {/* <BadgeCarousel /> */}
+          <LastActivities />
+        </Box>
+        {/* <Footer /> */}
+      </Theme>
+    </ApolloProvider>
   );
 }
--- src/components/LastActivities.tsx
+++ src/components/LastActivities.tsx
@@ -1,8 +1,23 @@
+import { gql, useSubscription } from "@apollo/client";
 import { Box, Flex, Link, Button, Text } from "@radix-ui/themes";
 import Image from "next/image";
 
 const LastActivities = () => {
-    
-}
+  const LOGS_SUBSCRIPTION = gql`
+    subscription LogsSubscription {
+      logs(order_by: { block_timestamp: desc }, limit: 6) {
+        transaction_hash
+        decoded
+        block_timestamp
+      }
+    }
+  `;
+
+  const {data, loading, error} = useSubscription(LOGS_SUBSCRIPTION);
+
+  console.log("data", data);
+
+  return <Box></Box>;
+};
 
 export default LastActivities;
